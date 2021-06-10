b807990e5edba2491e81af11c8e40a464b50b6cc
Alex <alexandre.salvatore@hardis.fr>
2021-06-10 10:53:22 +0200

    added mechanism au server synchronization

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,18 @@
+import * as localforage from "localforage";
 const SERVER_URL = "http://localhost:8888/freemarket/";
 const MONEY = "";
+
+export const dbSaveBlocks = (blocks) => {
+  return localforage.setItem("chain", JSON.stringify(blocks));
+};
+
+export const dbFetchBlocks = (callback) => {
+  return localforage.getItem("chain", (err, data) => {
+    if (err || !data) return callback([]);
+
+    callback(JSON.parse(data));
+  });
+};
 
 export const wsGetAll = () => {
   const formData = new FormData();
@@ -11,9 +24,10 @@ export const wsGetAll = () => {
   }).then((response) => response.json());
 };
 
-export const wsPostBlock = (block) => {
+export const wsPostBlock = (block, ts) => {
   const formData = new FormData();
   formData.append("money", MONEY);
+  formData.append("ts", ts);
   formData.append("action", "addBlock");
   formData.append("block", JSON.stringify(block));
   return fetch(SERVER_URL, {
--- src/store/actions/chain-actions.js
+++ src/store/actions/chain-actions.js
@@ -1,5 +1,11 @@
-import { Block, Blockchain, Transaction } from "@asalvatore/microchain";
-import { wsGetAll, wsPostBlock, wsPostTX } from "../../services";
+import { Blockchain } from "@asalvatore/microchain";
+import {
+  wsGetAll,
+  wsPostBlock,
+  wsPostTX,
+  dbSaveBlocks,
+  dbFetchBlocks,
+} from "../../services";
 
 export const CHAIN_ADD_BLOCK = "CHAIN_ADD_BLOCK";
 export const CHAIN_IS_MINING = "CHAIN_IS_MINING";
@@ -14,77 +20,82 @@ const saveChain = () => {
 export const initChain = () => {
   // Try to fetch the chain
   return (dispatch) => {
-    wsGetAll().then((data) => {
-      const blocks = data.result.chain ? data.result.chain : [];
-      const pendingTX = data.result.pendingTX ? data.result.pendingTX : [];
+    // We fech the block from the local DB
+    dbFetchBlocks((blocksDB) => {
+      wsGetAll().then((data) => {
+        let blocks = data.result.chain ? data.result.chain : [];
+        blocks = [...blocks, ...blocksDB];
+        const pendingTX = data.result.pendingTX ? data.result.pendingTX : [];
 
-      const blockchain = Blockchain.init(
-        {
-          BLOCK_HASH_METHOD: "MD5",
-          BLOCK_MIN_DIFFICULTY: 3,
-          BLOCK_MAX_DIFFICULTY: 3,
-          TX_CONTENT_EXPIRATION_HOURS: 12,
-          MONEY_BY_BLOCK: 15,
-          MONEY_BY_KO: 1.2,
-          TX_CONTENT_MAX_SIZE_KO: 200,
-          GENESIS_BLOCK: {
-            publisher:
+        const blockchain = Blockchain.init(
+          {
+            BLOCK_HASH_METHOD: "MD5",
+            BLOCK_MIN_DIFFICULTY: 3,
+            BLOCK_MAX_DIFFICULTY: 3,
+            TX_CONTENT_EXPIRATION_HOURS: 12,
+            MONEY_BY_BLOCK: 15,
+            MONEY_BY_KO: 1.2,
+            TX_CONTENT_MAX_SIZE_KO: 200,
+            GENESIS_BLOCK: {
+              publisher:
+                "04820be6a65e928d52e92b8bfe7827de7a09d3afa1356ef81f6f8528b44ba84393d32b44e4590fa9ca6b9576a6d7f2f0467af33d8f68f83e1359a8e4981f4ed5f6",
+              ts: 0,
+              nonce: 7441,
+            },
+            FOUNDERS:
               "04820be6a65e928d52e92b8bfe7827de7a09d3afa1356ef81f6f8528b44ba84393d32b44e4590fa9ca6b9576a6d7f2f0467af33d8f68f83e1359a8e4981f4ed5f6",
           },
-          FOUNDERS:
-            "04820be6a65e928d52e92b8bfe7827de7a09d3afa1356ef81f6f8528b44ba84393d32b44e4590fa9ca6b9576a6d7f2f0467af33d8f68f83e1359a8e4981f4ed5f6",
-        },
-        blocks.length !== 0 ? blocks : null
-      );
+          blocks.length !== 0 ? blocks : null
+        );
 
-      // If blockmem == NULL we generated the genensis block
-      // if (!blocksMem) saveChain();
+        // Add genesis
+        const genesis = blockchain.lastBlock;
 
-      // Add genesis
-      const genesis = blockchain.lastBlock;
-      console.log(genesis);
-      if (blocks.length === 0) {
-        wsPostBlock(genesis).then((data) => console.log(data));
-      }
-      //dispatch({ type: CHAIN_ADD_BLOCK, payload: { block: genesis } });
-
-      blockchain.longestBlockchain.forEach((block) =>
-        dispatch({ type: CHAIN_ADD_BLOCK, payload: { block } })
-      );
-
-      pendingTX.forEach((tx) =>
-        dispatch({ type: CHAIN_ADD_PENDING_TX, payload: { tx } })
-      );
-
-      /*
-      const txHash = [];
-      blockchain.longestBlockchain.forEach((block) => {
-        const blockObj = new Block(block);
-        const result = blockchain.addBlock(blockObj);
-        if (result && blockObj.transactions) {
-          JSON.parse(blockObj.transactions).forEach((tx) => {
-            const txObj = new Transaction(tx);
-            txHash.push(txObj.hash);
-          });
+        if (blocks.length === 0) {
+          wsPostBlock(genesis).then((data) => console.log(data));
         }
-      });
+        //dispatch({ type: CHAIN_ADD_BLOCK, payload: { block: genesis } });
 
-      pendingTX.forEach((tx) => {
-        const txObj = new Transaction(tx);
-        if (txHash.findIndex((hash) => hash === txObj.hash) >= 0) return;
-        dispatch({ type: CHAIN_ADD_PENDING_TX, payload: { tx } });
-      });*/
+        blockchain.longestBlockchain.forEach((block) =>
+          dispatch({ type: CHAIN_ADD_BLOCK, payload: { block } })
+        );
+
+        pendingTX.forEach((tx) =>
+          dispatch({ type: CHAIN_ADD_PENDING_TX, payload: { tx } })
+        );
+
+        console.log(blockchain.longestBlockchain);
+        console.log(blockchain.chain);
+        dbSaveBlocks(blockchain.chain);
+      });
     });
   };
 };
 
 export const addBlock = (block) => {
   return (dispatch) => {
+    const lastTs = Blockchain.getInstance().lastBlock.ts;
     const result = Blockchain.getInstance().addBlock(block);
     if (!result) return;
-    //saveChain();
-    wsPostBlock(block).then((data) => console.log(data));
-    dispatch({ type: CHAIN_ADD_BLOCK, payload: { block } });
+
+    wsPostBlock(block, lastTs).then((data) => {
+      const blocks = data.result.chain ? data.result.chain : [];
+      const pendingTX = data.result.pendingTX ? data.result.pendingTX : [];
+
+      // Ajout des nouveaux block
+      blocks.forEach((newBlock) => {
+        const result = Blockchain.getInstance().addBlock(newBlock);
+        if (result) {
+          dispatch({ type: CHAIN_ADD_BLOCK, payload: { newBlock } });
+        }
+      });
+      //Ajout des pending
+      pendingTX.forEach((tx) =>
+        dispatch({ type: CHAIN_ADD_PENDING_TX, payload: { tx } })
+      );
+
+      dbSaveBlocks(blockchain.chain);
+    });
   };
 };
 
